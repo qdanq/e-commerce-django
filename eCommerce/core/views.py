30011572f510778aff48264b08d8d2209e96fa00
qqbeach <danildzekelev@gmail.com>
2023-12-19 15:52:54 +0200

    Create signup form and signup.html template.

--- eCommerce/core/views.py
+++ eCommerce/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from item.models import Category, Product
+from .forms import SignupForm
 
 def index(request):
     products = Product.objects.filter(is_sold=False)
@@ -8,4 +8,11 @@ def index(request):
     return render(request, 'index.html', {
         'products': products,
         'categories': categories,
+        })
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'signup.html', {
+        'form': form,
         })
