9eef088d447b65034b7445d340607caddfd32c63
qqbeach <danildzekelev@gmail.com>
2023-12-19 18:19:36 +0200

    Make working new item form.

--- eCommerce/item/views.py
+++ eCommerce/item/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from .models import Product
+from .forms import NewProductForm
 
 def detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     
     return render(request, 'detail.html', {
         'product': product,
+        })
+
+
+@login_required
+def new(request):
+    form = NewProductForm()
+
+    return render(request, 'form.html', {
+        'form': form
         })
