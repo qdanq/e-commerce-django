06cb4b7553c45f444486c8d1b6fdf2988539c148
qqbeach <danildzekelev@gmail.com>
2023-12-19 14:20:00 +0200

    Rename item model to product. make migrations

--- eCommerce/item/models.py
+++ eCommerce/item/models.py
@@ -10,12 +10,15 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Item(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True, max_length=255)
     price = models.FloatField()
+    image = models.ImageField(upload_to='product_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
-    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='product', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    
 
