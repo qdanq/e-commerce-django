4b8bb281d3ebb169af6fd3589dad6027de602fdd
qqbeach <danildzekelev@gmail.com>
2023-12-19 16:25:35 +0200

    Make post method for forms.

--- eCommerce/core/views.py
+++ eCommerce/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Product
 from .forms import SignupForm
 
@@ -11,7 +11,15 @@ def index(request):
         })
 
 def signup(request):
-    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+
+            return redirect('/')
+    else:
+        form = SignupForm()
 
     return render(request, 'signup.html', {
         'form': form,
