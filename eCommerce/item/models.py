f5667f36f2cdd0eebb406c9fd1d3af894c7e8565
qqbeach <danildzekelev@gmail.com>
2023-12-19 10:01:20 +0200

    Create Item model.

--- eCommerce/item/models.py
+++ eCommerce/item/models.py
@@ -1,9 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     class Meta():
+        ordering = ['name',] # sort by alphabet
         verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True, max_length=255)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
