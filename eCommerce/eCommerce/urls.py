65d8b5d39d09a7fb713a9adf6d67922e7729c4d9
qqbeach <danildzekelev@gmail.com>
2023-12-19 16:02:25 +0200

    Fix signup error.

--- eCommerce/eCommerce/urls.py
+++ eCommerce/eCommerce/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 from core.views import index
+from core import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
