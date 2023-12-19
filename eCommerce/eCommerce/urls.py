fd16968beb0b024b86c4ea3ccc9f8189911607bb
qqbeach <danildzekelev@gmail.com>
2023-12-19 15:15:47 +0200

    Create detail page.

--- eCommerce/eCommerce/urls.py
+++ eCommerce/eCommerce/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,5 +23,6 @@ from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- eCommerce/item/views.py
+++ eCommerce/item/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Product
+
+def detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    
+    return render(request, 'item/detail.html', {
+        'product': product,
+        })
