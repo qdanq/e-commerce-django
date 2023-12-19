92db030bd60887892b9365b27e2aaf1259120fb7
qqbeach <danildzekelev@gmail.com>
2023-12-19 17:24:12 +0200

    Make login page.

--- eCommerce/core/forms.py
+++ eCommerce/core/forms.py
@@ -1,6 +1,10 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+     pass
+    
 
 class SignupForm(UserCreationForm):
     class Meta:
--- eCommerce/eCommerce/urls.py
+++ eCommerce/eCommerce/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from core.views import index, signup
 from core import views 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('items/', include('item.urls')),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
